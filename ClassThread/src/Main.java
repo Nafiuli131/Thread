c080b656eebc02e2984a9a9575781c5645c1770b
Nafiul Islam <nafiul.islam@synesisit.info>
2024-11-03 13:14:03 +0600

    join() method

--- ClassThread/src/Main.java
+++ ClassThread/src/Main.java
@@ -1,11 +1,13 @@
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Main thread starts --");
         ExtendedThread extendedThread = new ExtendedThread();
         extendedThread.setName("Extended thread");
         extendedThread.start();
+//        extendedThread.join();
+//        it means like first complete all extendedThread works,then go to the main thread
         for(int i=0;i<5;i++){
             System.out.println("Inside"+Thread.currentThread().getName());
             sleepOneSecond();
