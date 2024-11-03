460814d093d61ea6235adfaaa5af7f8719bf2675
Nafiul Islam <nafiul.islam@synesisit.info>
2024-11-03 11:51:44 +0600

    class thread

---
+++ ClassThread/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main thread starts --");
+        ExtendedThread extendedThread = new ExtendedThread();
+        extendedThread.start();
+        for(int i=0;i<5;i++){
+            System.out.println("Inside"+Thread.currentThread().getName());
+            sleepOneSecond();
+        }
+    }
+    private static void sleepOneSecond() {
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
